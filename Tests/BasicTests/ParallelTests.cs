aed3edaf6aa0d90db93d3834bd0cf1b0c068970e
Corbo12 <shaun.orssaud@epitech.eu>
2023-04-26 14:59:26 +0200

    MDF split tests into different files

--- Tests/BasicTests/ParallelTests.cs
+++ Tests/BasicTests/ParallelTests.cs
@@ -4,52 +4,6 @@ using NUnit.Framework;
 namespace Tests.ParallelTests
 {
     [TestFixture]
-    public class DownloadTests : DistTest
-    {
-        [Test]
-        public void ThreeNodeDownloads()
-        {
-            ParallelDownload(3, 5000.MB());
-        }
-        [Test]
-        public void FiveNodeDownloads()
-        {
-            ParallelDownload(5, 1000.MB());
-        }
-        [Test]
-        public void TenNodeDownloads()
-        {
-            ParallelDownload(10, 256.MB());
-        }
-
-        void ParallelDownload(int numberOfNodes, ByteSize filesize)
-        {
-            var group = SetupCodexNodes(numberOfNodes).BringOnline();
-            var host = SetupCodexNodes(1).BringOnline()[0];
-
-            foreach (var node in group)
-            {
-                host.ConnectToPeer(node);
-            }
-
-            var testFile = GenerateTestFile(filesize);
-            var contentId = host.UploadFile(testFile);
-            var list = new List<Task<TestFile?>>();
-            
-            foreach (var node in group)
-            {
-                list.Add(Task.Run(() => { return node.DownloadContent(contentId); }));
-            }
-
-            Task.WaitAll(list.ToArray());
-            foreach (var task in list)
-            {
-                testFile.AssertIsEqual(task.Result);
-            }
-        }
-    }
-
-    [TestFixture]
     public class UploadTests : DistTest
     {
         [Test]
@@ -97,21 +51,6 @@ namespace Tests.ParallelTests
             {
                 testfiles[i].AssertIsEqual(downloads[i].Result);
             }
-        }
-    }
-    [TestFixture]
-    public class MixedTests : DistTest
-    {
-        [Test]
-        public void OneDownloadOneUpload()
-        {
-        }
-
-        public void ThreeDownloadTwoUpload()
-        {
-        }
-        public void FiveDownloadFiveUpload()
-        {
         }
     }
 }
