5da06e08f3d3c23848da52833b5059dbe0f6e651
Corbo12 <shaun.orssaud@epitech.eu>
2023-06-07 14:14:03 +0200

    ADD simple upload / download tests

--- Tests/MembershipChangeTest/MixedTests.cs
+++ Tests/MembershipChangeTest/MixedTests.cs
@@ -6,18 +6,17 @@ namespace Tests.MembershipChangeTests
     [TestFixture]
     public class MixedMembershipChangeTests : DistTest
     {
-        [TestCase(1, 100, 5, 0)]
-        [TestCase(1, 100, 0, 5)]
-        [TestCase(1, 100, 5, 5)]
+        [TestCase(1, 5, 0, 0)]
 
         [UseLongTimeouts]
-        public void MixedMembershipChange(int numberOfNodes, int filesize, int numberOfNodesToAdd = 0, int numberOfNodesToRemove = 0)
+        public void MixedMembershipChange(int numberOfNodes, int filesize, int numberOfNodesToAdd, int numberOfNodesToRemove)
         {
             // Creating the node groups
             ICodexNodeGroup? toAdd = null;
             ICodexNodeGroup? toAddSecondary = null;
             ICodexNodeGroup? toRemove = null;
             var group = SetupCodexNodes(numberOfNodes);
+            
             if (numberOfNodesToAdd != 0) {
                 toAdd = SetupCodexNodes(numberOfNodesToAdd);
                 toAddSecondary = SetupCodexNodes(numberOfNodesToAdd);
@@ -35,12 +34,18 @@ namespace Tests.MembershipChangeTests
                 foreach (var node in toRemove)
                     host.ConnectToPeer(node);
 
+            var testFile = GenerateTestFile(filesize.MB());
+            var contentId = host.UploadFile(testFile);
+
             var testfiles = new List<TestFile>();
             var contentIds = new List<Task<ContentId>>();
+
+            var downloads = new List<Task<TestFile?>>();
 
             // Start adding and dropping nodes
             for (var i = 0; (toAdd != null && i < toAdd.Count()) || (toRemove != null && i < toRemove.Count()); i++)
             {
+                Log($"Iteration {i}");
                 if (toAdd != null && i < toAdd.Count())
                     Task.Run(() => { host.ConnectToPeer(toAdd[i]); });
                 if (toRemove != null && i < toRemove.Count())
@@ -55,27 +60,18 @@ namespace Tests.MembershipChangeTests
                 contentIds.Add(Task.Run(() => { return host.UploadFile(testfiles[n]); }));
             }
 
-            // Wait for the upload to finish
-            Task.WaitAll(contentIds.ToArray());
-
-            // Start adding and dropping nodes again
-            for (var i = 0; (toAddSecondary != null && i < toAddSecondary.Count()) || (toAdd != null && i < toAdd.Count()); i++)
-            {
-                if (toAddSecondary != null && i < toAddSecondary.Count())
-                    Task.Run(() => { host.ConnectToPeer(toAddSecondary[i]); });
-                if (toAdd != null && i < toAdd.Count())
-                    Task.Run(() => { toAdd[i].BringOffline(); });
-            }
-            // Download the files
-            var downloads = new List<Task<TestFile?>>();
+            // Start the download for each node in the main group
             for (int i = 0; i < group.Count(); i++)
             {
                 var n = i;
-                downloads.Add(Task.Run(() => { return group[n].DownloadContent(contentIds[n].Result); }));
+                downloads.Add(Task.Run(() => { return group[n].DownloadContent(contentId); }));
             }
 
             // Wait for the download to finish
             Task.WaitAll(downloads.ToArray());
+
+            // Wait for the upload to finish
+            Task.WaitAll(contentIds.ToArray());
 
             // Assert that the files are intact
             for (int i = 0; i < group.Count(); i++)
