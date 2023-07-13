1f8fc52cb1265abb7d30f6433e8bc6606d6d69a4
benbierens <thatbenbierens@gmail.com>
2023-07-13 09:20:24 +0200

    Adds three-client test to check discovery relaying

---
+++ Tests/BasicTests/ThreeClientTest.cs
@@ -0,0 +1,24 @@
+﻿using DistTestCore;
+using NUnit.Framework;
+
+namespace Tests.BasicTests
+{
+    [TestFixture]
+    public class ThreeClientTest : AutoBootstrapDistTest
+    {
+        [Test]
+        public void ThreeClient()
+        {
+            var primary = SetupCodexNode();
+            var secondary = SetupCodexNode();
+
+            var testFile = GenerateTestFile(10.MB());
+
+            var contentId = primary.UploadFile(testFile);
+
+            var downloadedFile = secondary.DownloadContent(contentId);
+
+            testFile.AssertIsEqual(downloadedFile);
+        }
+    }
+}
