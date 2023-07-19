c744fd12c3b23fcc9f0b3acf7d9b9cda7c88a878
benbierens <thatbenbierens@gmail.com>
2023-07-19 08:46:10 +0200

    Rigs sh scripts for debugging of continuous tests.

--- CodexNetDeployer/deploy-continuous-testnet.sh
+++ CodexNetDeployer/deploy-continuous-testnet.sh
@@ -3,10 +3,10 @@ dotnet run \
     --kube-namespace=codex-continuous-tests \
     --nodes=5 \
     --validators=3 \
+    --log-level=Trace \
     --storage-quota=2048 \
     --storage-sell=1024 \
     --min-price=1024 \
     --max-collateral=1024 \
     --max-duration=3600000 \
     --block-ttl=120
-
--- ContinuousTests/run.sh
+++ ContinuousTests/run.sh
@@ -1,4 +1,5 @@
 dotnet run \
     --kube-config=/opt/kubeconfig.yaml \
     --codex-deployment=codex-deployment.json \
+    --keep=1 \
     --stop=1 
