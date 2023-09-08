d074f64c422b66819e9566c7cbefabc7d0db12b4
benbierens <thatbenbierens@gmail.com>
2023-09-08 14:03:47 +0200

    a

--- Tests/DownloadConnectivityTests/FullyConnectedDownloadTests.cs
+++ Tests/DownloadConnectivityTests/FullyConnectedDownloadTests.cs
@@ -26,8 +26,8 @@ namespace Tests.DownloadConnectivityTests
         [Test]
         [Combinatorial]
         public void FullyConnectedDownloadTest(
-            [Values(1, 3, 5)] int numberOfNodes,
-            [Values(1, 10)] int sizeMBs)
+            [Values(3, 5)] int numberOfNodes,
+            [Values(10, 80)] int sizeMBs)
         {
             SetupCodexNodes(numberOfNodes);
 
