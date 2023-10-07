75e263be319d8ae722333ac1de9b811f5570ff8e
benbierens <thatbenbierens@gmail.com>
2023-10-07 07:47:54 +0200

    Try with script

---
+++ Tests/CodexContinuousTests/deploy-and-run.sh
@@ -0,0 +1,35 @@
+replication=10
+
+echo "Deploying..."
+cd ../../Tools/CodexNetDeployer
+for i in {0..$replication}
+do
+    dotnet run \
+    --kube-config=/opt/kubeconfig.yaml \
+    --kube-namespace=codex-continuous-tests-$i \
+    --deploy-file=codex-deployment-$i.json \
+    --nodes=5 \
+    --validators=3 \
+    --log-level=Trace \
+    --storage-quota=2048 \
+    --storage-sell=1024 \
+    --min-price=1024 \
+    --max-collateral=1024 \
+    --max-duration=3600000 \
+    --block-ttl=180 \
+    --block-mi=120 \
+    --block-mn=10000 \
+    --metrics=1 \
+    --check-connect=1
+done
+echo "Starting tests..."
+cd ../../Tests/CodexContinousTests
+for i in {0..$replication}
+do
+    screen -d -m dotnet run \
+    --kube-config=/opt/kubeconfig.yaml \
+    --codex-deployment=codex-deployment-$i.json \
+    --keep=1 \
+    --stop=1 \
+    --target-duration=172800 # 48 hours
+done
