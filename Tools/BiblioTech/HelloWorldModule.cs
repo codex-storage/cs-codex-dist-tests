7179c7046338a4d21873bb6f0bcfb5e2682553bd
benbierens <thatbenbierens@gmail.com>
2023-10-18 09:10:04 +0200

    Sets up an echo command

---
+++ Tools/BiblioTech/HelloWorldModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+
+namespace BiblioTech
+{
+    public class HelloWorldModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+        {
+            return ReplyAsync("I say: " + echo);
+        }
+    }
+}
--- Tools/BiblioTech/Program.cs
+++ Tools/BiblioTech/Program.cs
@@ -1,8 +1,29 @@
-﻿public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Starting Codex Discord bot BiblioTech...");
+﻿using Discord;
+using Discord.WebSocket;
 
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient client;
+
+    public async Task MainAsync()
+    {
+        client = new DiscordSocketClient();
+
+        client.Log += Log;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = "token";
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        await Task.Delay(-1);
+    }
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
     }
 }
