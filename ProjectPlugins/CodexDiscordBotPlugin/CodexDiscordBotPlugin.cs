b74349cc68e5f123299696681f8cdf9da8d02e0c
benbierens <thatbenbierens@gmail.com>
2023-10-24 09:51:29 +0200

    Setting up codex discord bot plugin

---
+++ ProjectPlugins/CodexDiscordBotPlugin/CodexDiscordBotPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Core;
+using KubernetesWorkflow;
+
+namespace CodexDiscordBotPlugin
+{
+    public class CodexDiscordBotPlugin : IProjectPlugin, IHasLogPrefix, IHasMetadata
+    {
+        private readonly IPluginTools tools;
+
+        public CodexDiscordBotPlugin(IPluginTools tools)
+        {
+            this.tools = tools;
+        }
+
+        public string LogPrefix => "(DiscordBot) ";
+
+        public void Announce()
+        {
+            tools.GetLog().Log($"Codex DiscordBot (BiblioTech) loaded.");
+        }
+
+        public void AddMetadata(IAddMetadata metadata)
+        {
+            metadata.Add("codexdiscordbotid", "todo");
+        }
+
+        public void Decommission()
+        {
+        }
+
+        public RunningContainer Deploy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectPlugins/CodexDiscordBotPlugin/CoreInterfaceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using KubernetesWorkflow;
+
+namespace CodexDiscordBotPlugin
+{
+    public static class CoreInterfaceExtensions
+    {
+        public static RunningContainer DeployMetricsCollector(this CoreInterface ci)
+        {
+            return Plugin(ci).Deploy();
+        }
+
+        private static CodexDiscordBotPlugin Plugin(CoreInterface ci)
+        {
+            return ci.GetPlugin<CodexDiscordBotPlugin>();
+        }
+    }
+}
