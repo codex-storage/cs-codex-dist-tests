c348ca98490e4d4d84e3251a7e8eeafa0b7c490d
benbierens <thatbenbierens@gmail.com>
2023-10-31 11:22:59 +0100

    sets up test starter tool

---
+++ Tools/TestClusterStarter/ClusterTestSpec.cs
@@ -0,0 +1,42 @@
+﻿using KubernetesWorkflow;
+
+namespace TestClusterStarter
+{
+    public class ClusterTestSetup
+    {
+        public ClusterTestSetup(ClusterTestSpec[] specs)
+        {
+            Specs = specs;
+        }
+
+        public ClusterTestSpec[] Specs { get; }
+    }
+
+    public class ClusterTestSpec
+    {
+        public ClusterTestSpec(string name, string filter, int replication, int durationSeconds, string? codexImageOverride)
+        {
+            Name = name;
+            Filter = filter;
+            Replication = replication;
+            DurationSeconds = durationSeconds;
+            CodexImageOverride = codexImageOverride;
+        }
+
+        public string Name { get; }
+        public string Filter { get; }
+        public int Replication { get; }
+        public int DurationSeconds { get; }
+        public string? CodexImageOverride { get; }
+    }
+
+    public class ClusterTestDeployment
+    {
+        public ClusterTestDeployment(RunningContainer[] containers)
+        {
+            Containers = containers;
+        }
+
+        public RunningContainer[] Containers { get; }
+    }
+}
