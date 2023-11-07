024448ae9431b4a170b1ff6d01c9546a8976fbd7
gmega <giuliano.mega@gmail.com>
2023-11-07 15:35:19 -0300

    customize run script and codex recipe

--- ProjectPlugins/CodexPlugin/CodexContainerRecipe.cs
+++ ProjectPlugins/CodexPlugin/CodexContainerRecipe.cs
@@ -26,7 +26,7 @@ namespace CodexPlugin
 
         protected override void Initialize(StartupConfig startupConfig)
         {
-            SetResourcesRequest(milliCPUs: 100, memory: 100.MB());
+            SetResourcesRequest(milliCPUs: 100, memory: 12.GB());
             //SetResourceLimits(milliCPUs: 4000, memory: 12.GB());
 
             SetSchedulingAffinity(notIn: "false");
--- Tests/CodexContinuousTests/deploy-and-run.sh
+++ Tests/CodexContinuousTests/deploy-and-run.sh
@@ -1,55 +1,84 @@
+#!/usr/bin/env bash
 set -e
 
-replication=$DNR_REP
-name=$DNR_NAME
-filter=$DNR_FILTER
-duration=$DNR_DURATION
+kubeconfig="/opt/kubeconfig.yaml"
+replication=5
+name=profiling-two-client-tests
+filter=TwoClient
+cleanup=1
 
-echo "Deploying..."
-cd ../../Tools/CodexNetDeployer
-for i in $( seq 0 $replication)
-do
-    dotnet run \
-    --deploy-name=codex-continuous-$name-$i \
-    --kube-config=/opt/kubeconfig.yaml \
-    --kube-namespace=codex-continuous-$name-tests-$i \
-    --deploy-file=codex-deployment-$name-$i.json \
-    --nodes=5 \
-    --validators=3 \
-    --log-level=Trace \
-    --storage-quota=20480 \
-    --storage-sell=1024 \
-    --min-price=1024 \
-    --max-collateral=1024 \
-    --max-duration=3600000 \
-    --block-ttl=99999999 \
-    --block-mi=99999999 \
-    --block-mn=100 \
-    --metrics-endpoints=1 \
-    --metrics-scraper=1 \
-    --check-connect=1 \
-    -y
+usage() { 
+    echo "Usage: $0 [-m <deploy|run>] [-r <replication>] [-k <kubeconfig>] [-c <cleanup>]" 1>&2
+    exit 1
+}
 
-    cp codex-deployment-$name-$i.json ../../Tests/CodexContinuousTests
-done
-echo "Starting tests..."
-cd ../../Tests/CodexContinuousTests
-for i in $( seq 0 $replication)
-do
-    screen -d -m dotnet run \
-    --kube-config=/opt/kubeconfig.yaml \
-    --codex-deployment=codex-deployment-$name-$i.json \
-    --log-path=/var/log/codex-continuous-tests/logs-$name-$i \
-    --data-path=data-$name-$i \
-    --keep=1 \
-    --stop=1 \
-    --filter=$filter \
-    --cleanup=1 \
-    --full-container-logs=1 \
-    --target-duration=$duration
-
-    sleep 30
+while getopts ":r:k:m:c:" o; do
+    case "$o" in
+        r)
+            replication="$OPTARG"
+            ;;
+        k)
+            kubeconfig="$OPTARG"
+            ;;
+        m)
+            mode="$OPTARG"
+            ;;
+        c)
+            cleanup="$OPTARG"
+            ;;
+        *)
+            usage
+            ;;
+    esac
 done
 
-echo "Done! Sleeping indefinitely..."
-while true; do sleep 1d; done
+echo "Replication: $replication; Kube config: $kubeconfig; Mode: $mode; Cleanup: $cleanup"
+
+if [ -z "$mode" ] || [ "$mode" == "deploy" ]; then
+    echo "Deploying..."
+    cd ../../Tools/CodexNetDeployer
+    for i in $(seq 0 "$replication")
+    do
+        dotnet run \
+        --deploy-name=codex-continuous-"$name-$i" \
+        --kube-config="$kubeconfig" \
+        --kube-namespace=codex-continuous-tests-"$name-$i" \
+        --deploy-file=codex-deployment-"$name-$i".json \
+        --nodes=5 \
+        --validators=3 \
+        --log-level=Trace \
+        --storage-quota=20480 \
+        --storage-sell=1024 \
+        --min-price=1024 \
+        --max-collateral=1024 \
+        --max-duration=3600000 \
+        --block-ttl=99999999 \
+        --block-mi=99999999 \
+        --block-mn=100 \
+        --metrics=1 \
+        --check-connect=1 \
+        -y
+
+        cp codex-deployment-"$name-$i".json ../../Tests/CodexContinuousTests
+    done
+    exit 0
+fi
+
+if [ -z "$mode" ] || [ "$mode" == "run" ]; then
+    echo "Starting tests..."
+    cd ../../Tests/CodexContinuousTests
+    for i in $(seq 0 "$replication")
+    do
+        screen -d -m dotnet run \
+        --kube-config="${kubeconfig}" \
+        --codex-deployment=codex-deployment-"$name-$i".json \
+        --log-path=logs-"$i" \
+        --data-path=data-"$i" \
+        --keep=1 \
+        --stop=1 \
+        --filter="$filter" \
+        --cleanup="$cleanup" \
+        --full-container-logs=1 \
+        --target-duration=172800 # 48 hours
+    done
+fi
