258731c53f1062d1f49928ec1b437320aee00125
gmega <giuliano.mega@gmail.com>
2023-11-24 16:07:56 -0300

    set profiling defaults

--- Tools/CodexNetDeployer/deploy-continuous-testnet.sh
+++ Tools/CodexNetDeployer/deploy-continuous-testnet.sh
@@ -1,19 +1,19 @@
 dotnet run \
     --project "${DEPLOYMENT_CODEXNETDEPLOYER_PATH:-Tools/CodexNetDeployer}" \
-    --deploy-name="${DEPLOYMENT_NAME:-codex-continuous-test-deployment}" \
+    --deploy-name="${DEPLOYMENT_NAME:-codex-profiling-two-client-test}" \
     --kube-config="${KUBECONFIG:-/opt/kubeconfig.yaml}" \
-    --kube-namespace="${DEPLOYMENT_NAMESPACE:-codex-continuous-tests}" \
+    --kube-namespace="${DEPLOYMENT_NAMESPACE:-codex-profiling-two-client-test}" \
     --deploy-file="${DEPLOYMENT_FILE:-codex-deployment.json}" \
-    --nodes=${DEPLOYMENT_NODES:-5} \
-    --validators=${DEPLOYMENT_VALIDATORS:-3} \
+    --nodes=${DEPLOYMENT_NODES:-2} \
+    --validators=${DEPLOYMENT_VALIDATORS:-1} \
     --log-level="${CODEX_LOG_LEVEL:-Trace}" \
-    --storage-quota=${CODEX_STORAGE_QUOTA:-2048} \
+    --storage-quota=${CODEX_STORAGE_QUOTA:-60000} \
     --storage-sell=${CODEX_STORAGE_SELL:-1024} \
     --min-price=${CODEX_MIN_PRICE:-1024} \
     --max-collateral=${CODEX_MAX_COLLATERAL:-1024} \
     --max-duration=${CODEX_MAX_DURATION:-3600000} \
-    --block-ttl=${CODEX_BLOCK_TTL:-180} \
-    --block-mi=${CODEX_BLOCK_MI:-120} \
+    --block-ttl=${CODEX_BLOCK_TTL:-9999999} \
+    --block-mi=${CODEX_BLOCK_MI:-9999999} \
     --block-mn=${CODEX_BLOCK_MN:-10000} \
     --metrics-endpoints=${CODEX_METRICS:-1} \
     --metrics-scraper=${CODEX_METRICS:-1} \
