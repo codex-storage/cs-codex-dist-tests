2e299e7a8d2a91de74fb0a5735af35208a705685
gmega <giuliano.mega@gmail.com>
2024-01-22 10:01:44 -0300

    add relpath to launch script

--- Tools/CodexNetDeployer/deploy-chronos-v4-testnet.sh
+++ Tools/CodexNetDeployer/deploy-chronos-v4-testnet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DEPLOYMENT_NAME="-chronos-v4"
-export DEPLOYMENT_NAMESPACE="-chronos-v4-testnet"
+export DEPLOYMENT_NAMESPACE="chronos-v4-testnet"
 export CODEX_STORAGE_QUOTA=60000
 export CODEX_BLOCK_TTL=9999999
 export CODEX_BLOCK_MI=9999999
