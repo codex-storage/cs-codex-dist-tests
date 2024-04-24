a6379d02f1f124ccff2a99787123a45d3f87196d
Ben <thatbenbierens@gmail.com>
2024-04-24 09:41:05 +0200

    Adds test for upload/download large file with single node

---
+++ Tests/CodexTests/ScalabilityTests/OneClientLargeFileTests.cs
@@ -0,0 +1,36 @@
+﻿using CodexPlugin;
+using DistTestCore;
+using NUnit.Framework;
+using Utils;
+
+namespace CodexTests.ScalabilityTests
+{
+    [TestFixture]
+    public class OneClientLargeFileTests : CodexDistTest
+    {
+        [Test]
+        [Combinatorial]
+        [UseLongTimeouts]
+        public void OneClientLargeFile([Values(
+            256,
+            512,
+            1024, // GB
+            2048,
+            4096,
+            8192,
+            16384,
+            32768,
+            65536,
+            131072
+        )] int sizeMb)
+        {
+            var testFile = GenerateTestFile(sizeMb.MB());
+
+            var node = AddCodex(s => s.WithLogLevel(CodexLogLevel.Warn));
+            var contentId = node.UploadFile(testFile);
+            var downloadedFile = node.DownloadContent(contentId);
+
+            testFile.AssertIsEqual(downloadedFile);
+        }
+    }
+}
