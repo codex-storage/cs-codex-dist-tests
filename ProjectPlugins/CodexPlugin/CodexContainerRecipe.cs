2902f6baabb36b666a80576d89425aba057c2903
Ben <thatbenbierens@gmail.com>
2024-05-01 10:05:46 +0200

    latest master image of Codex

--- ProjectPlugins/CodexPlugin/CodexContainerRecipe.cs
+++ ProjectPlugins/CodexPlugin/CodexContainerRecipe.cs
@@ -7,7 +7,7 @@ namespace CodexPlugin
 {
     public class CodexContainerRecipe : ContainerRecipeFactory
     {
-        private const string DefaultDockerImage = "codexstorage/nim-codex:sha-bb0829e-dist-tests";
+        private const string DefaultDockerImage = "codexstorage/nim-codex:sha-c58d4d7-dist-tests";
         public const string ApiPortTag = "codex_api_port";
         public const string ListenPortTag = "codex_listen_port";
         public const string MetricsPortTag = "codex_metrics_port";
