01ee514c730d0f5b1baf8b09ab5ff7fbc797be53
Ben <thatbenbierens@gmail.com>
2024-06-27 10:44:37 +0200

    Implements known username replacement

--- Tools/BiblioTech/Rewards/ChainEventsSender.cs
+++ Tools/BiblioTech/Rewards/ChainEventsSender.cs
@@ -21,11 +21,14 @@ namespace BiblioTech.Rewards
             {
                 await Task.Run(async () =>
                 {
+                    var users = Program.UserRepo.GetAllUserData();
+
                     foreach (var e in eventsOverview)
                     {
                         if (!string.IsNullOrEmpty(e))
                         {
-                            await eventsChannel.SendMessageAsync(e);
+                            var @event = ApplyReplacements(users, e);
+                            await eventsChannel.SendMessageAsync(@event);
                             await Task.Delay(3000);
                         }
                     }
@@ -35,6 +38,27 @@ namespace BiblioTech.Rewards
             {
                 log.Error("Failed to process chain events: " + ex);
             }
+        }
+
+        private string ApplyReplacements(UserData[] users, string msg)
+        {
+            var result = ApplyUserAddressReplacements(users, msg);
+            return result;
+        }
+
+        private string ApplyUserAddressReplacements(UserData[] users, string msg)
+        {
+            foreach (var user in users)
+            {
+                if (user.CurrentAddress != null &&
+                    !string.IsNullOrEmpty(user.CurrentAddress.Address) &&
+                    !string.IsNullOrEmpty(user.Name))
+                {
+                    msg = msg.Replace(user.CurrentAddress.Address, user.Name);
+                }
+            }
+
+            return msg;
         }
     }
 }
