5896735884995e6312c828370a2cdb8d2251176e
benbierens <thatbenbierens@gmail.com>
2024-10-11 10:30:07 +0200

    set up swarm test

---
+++ Tests/CodexTests/DownloadConnectivityTests/SwarmTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Utils;
+
+namespace CodexTests.DownloadConnectivityTests
+{
+    [TestFixture]
+    public class SwarmTests : AutoBootstrapDistTest
+    {
+        [Test]
+        [CreateTranscript("swarm_retransmit")]
+        public void DetectBlockRetransmits()
+        {
+            var nodes = StartCodex(10);
+            var file = GenerateTestFile(10.MB());
+            var cid = nodes[0].UploadFile(file);
+
+            var tasks = nodes.Select(n => Task.Run(() => n.DownloadContent(cid))).ToArray();
+            Task.WaitAll(tasks);
+        }
+    }
+}
