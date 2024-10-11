1bd84a48923dc9e4431f12bb57b4a32156770e9c
benbierens <thatbenbierens@gmail.com>
2024-10-11 11:20:59 +0200

    combinatorial swarm tests

--- Tests/CodexTests/DownloadConnectivityTests/SwarmTests.cs
+++ Tests/CodexTests/DownloadConnectivityTests/SwarmTests.cs
@@ -7,11 +7,15 @@ namespace CodexTests.DownloadConnectivityTests
     public class SwarmTests : AutoBootstrapDistTest
     {
         [Test]
+        [Combinatorial]
         [CreateTranscript("swarm_retransmit")]
-        public void DetectBlockRetransmits()
+        public void DetectBlockRetransmits(
+            [Values(1, 5, 10, 20)] int fileSize,
+            [Values(3, 5, 10, 20)] int numNodes
+        )
         {
-            var nodes = StartCodex(10);
-            var file = GenerateTestFile(10.MB());
+            var nodes = StartCodex(numNodes);
+            var file = GenerateTestFile(fileSize.MB());
             var cid = nodes[0].UploadFile(file);
 
             var tasks = nodes.Select(n => Task.Run(() => n.DownloadContent(cid))).ToArray();
