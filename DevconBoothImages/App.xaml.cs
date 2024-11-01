bb3f0fccd057f3191517f6ae563fd7b53dfa151a
benbierens <thatbenbierens@gmail.com>
2024-11-01 08:48:42 +0100

    set up plan

---
+++ DevconBoothImages/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DevconBoothImages
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ DevconBoothImages/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace DevconBoothImages
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // image
+            // upload
+            // clipboard info
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // clipboard info
+        }
+    }
+}
