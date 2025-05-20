8776591094f6e1649ac6fc3b7e03539c0ba17659
ThatBen <thatbenbierens@gmail.com>
2025-05-20 12:47:36 +0200

    wip

--- TraceContract/Output.cs
+++ TraceContract/Output.cs
@@ -1,16 +1,66 @@
-﻿using CodexContractsPlugin;
+﻿using System.Numerics;
+using CodexContractsPlugin.ChainMonitor;
 using CodexContractsPlugin.Marketplace;
+using Logging;
+using Utils;
 
 namespace TraceContract
 {
     public class Output
     {
-        public void LogRequest(Request request)
+        private readonly ILog log;
+
+        public Output(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void LogRequestCreated(RequestEvent requestEvent)
         {
             throw new NotImplementedException();
         }
 
-        public void LogEventOrCall<T>(T[] calls) where T : IHasRequestId, IHasBlock
+        public void LogRequestCancelled(RequestEvent requestEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogRequestFailed(RequestEvent requestEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogRequestFinished(RequestEvent requestEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogRequestStarted(RequestEvent requestEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogSlotFilled(RequestEvent requestEvent, EthAddress host, BigInteger slotIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogSlotFreed(RequestEvent requestEvent, BigInteger slotIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogSlotReservationsFull(RequestEvent requestEvent, BigInteger slotIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogReserveSlotCalls(ReserveSlotFunction[] reserveSlotFunctions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteContractEvents()
         {
             throw new NotImplementedException();
         }
--- TraceContract/Program.cs
+++ TraceContract/Program.cs
@@ -1,5 +1,7 @@
-﻿using BlockchainUtils;
+﻿using System.Numerics;
+using BlockchainUtils;
 using CodexContractsPlugin;
+using CodexContractsPlugin.ChainMonitor;
 using CodexContractsPlugin.Marketplace;
 using Core;
 using GethPlugin;
@@ -11,7 +13,12 @@ namespace TraceContract
 {
     public class Input
     {
-        public string PurchaseId { get; } = "a7fe97dc32216aba0cbe74b87beb3f919aa116090dd5e0d48085a1a6b0080e82";
+        public string PurchaseId { get; } =
+            // expired:
+            //"a7fe97dc32216aba0cbe74b87beb3f919aa116090dd5e0d48085a1a6b0080e82";
+
+            // started:
+            "066df09a3a2e2587cfd577a0e96186c915b113d02b331b06e56f808494cff2b4";
     }
 
     public class Program
@@ -24,8 +31,13 @@ namespace TraceContract
 
         private readonly ILog log = new ConsoleLog();
         private readonly Input input = new();
-        private readonly Output output = new();
         private readonly Config config = new();
+        private readonly Output output;
+
+        public Program()
+        {
+            output = new(log);
+        }
 
         private void Run()
         {
@@ -45,7 +57,7 @@ namespace TraceContract
             var contracts = ConnectCodexContracts();
             
             var chainTracer = new ChainTracer(log, contracts, input, output);
-            chainTracer.TraceChainTimeline();
+            var requestTimeRange = chainTracer.TraceChainTimeline();
 
             Log("Done");
         }
@@ -93,26 +105,34 @@ namespace TraceContract
             this.output = output;
         }
 
-        public void TraceChainTimeline()
+        public TimeRange TraceChainTimeline()
         {
             var request = GetRequest();
             if (request == null) throw new Exception("Failed to find the purchase in the last week of transactions.");
-            output.LogRequest(request);
 
-            var requestTimeRange = new TimeRange(request.Block.Utc.AddMinutes(-1.0), DateTime.UtcNow);
-            var events = contracts.GetEvents(requestTimeRange);
+            var utc = request.Block.Utc.AddMinutes(-1.0);
+            var tracker = new ChainRequestTracker(output, input.PurchaseId);
+            var ignoreLog = new NullLog();
+            var chainState = new ChainState(ignoreLog, contracts, tracker, utc, false);
 
-            // Log calls to reserve slot for request
-            var calls = Filter(events.GetReserveSlotCalls());
-            output.LogEventOrCall(calls);
+            while (!tracker.IsFinished)
+            {
+                utc += TimeSpan.FromHours(1.0);
+                chainState.Update(utc);
+            }
 
-            // log all events.
-            var fulls = events.GetSlotReservationsFullEvents();
-            output.LogEventOrCall(Filter(fulls));
+            var requestTimeline = new TimeRange(request.Block.Utc.AddMinutes(-1.0), tracker.FinishUtc.AddMinutes(1.0));
 
+            // For this timeline, we log all the calls to reserve-slot.
+            var events = contracts.GetEvents(requestTimeline);
+            output.LogReserveSlotCalls(Filter(events.GetReserveSlotCalls()));
+
+            output.WriteContractEvents();
+
+            return requestTimeline;
         }
 
-        private T[] Filter<T>(T[] calls) where T : IHasRequestId
+        private ReserveSlotFunction[] Filter(ReserveSlotFunction[] calls)
         {
             return calls.Where(c => IsThisRequest(c.RequestId)).ToArray();
         }
@@ -146,24 +166,115 @@ namespace TraceContract
             return requestId.ToHex().ToLowerInvariant() == input.PurchaseId.ToLowerInvariant();
         }
 
-        private TimeRange LastHour()
+        private static TimeRange LastHour()
         {
             return new TimeRange(DateTime.UtcNow.AddHours(-1.0), DateTime.UtcNow);
         }
 
-        private TimeRange LastDay()
+        private static TimeRange LastDay()
         {
             return new TimeRange(DateTime.UtcNow.AddDays(-1.0), DateTime.UtcNow);
         }
 
-        private TimeRange LastWeek()
+        private static TimeRange LastWeek()
         {
             return new TimeRange(DateTime.UtcNow.AddDays(-7.0), DateTime.UtcNow);
         }
+    }
 
-        private void Log(string msg)
+    public class ChainRequestTracker : IChainStateChangeHandler
+    {
+        private readonly string requestId;
+        private readonly Output output;
+
+        public ChainRequestTracker(Output output, string requestId)
         {
-            log.Log(msg);
+            this.requestId = requestId.ToLowerInvariant();
+            this.output = output;
+        }
+
+        public bool IsFinished { get; private set; } = false;
+        public DateTime FinishUtc { get; private set; } = DateTime.MinValue;
+
+        public void OnError(string msg)
+        {
+        }
+
+        public void OnNewRequest(RequestEvent requestEvent)
+        {
+            if (IsMyRequest(requestEvent)) output.LogRequestCreated(requestEvent);
+        }
+
+        public void OnProofSubmitted(BlockTimeEntry block, string id)
+        {
+        }
+
+        public void OnRequestCancelled(RequestEvent requestEvent)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                IsFinished = true;
+                FinishUtc = requestEvent.Block.Utc;
+                output.LogRequestCancelled(requestEvent);
+            }
+        }
+
+        public void OnRequestFailed(RequestEvent requestEvent)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                IsFinished = true;
+                FinishUtc = requestEvent.Block.Utc;
+                output.LogRequestFailed(requestEvent);
+            }
+        }
+
+        public void OnRequestFinished(RequestEvent requestEvent)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                IsFinished = true;
+                FinishUtc = requestEvent.Block.Utc;
+                output.LogRequestFinished(requestEvent);
+            }
+        }
+
+        public void OnRequestFulfilled(RequestEvent requestEvent)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                output.LogRequestStarted(requestEvent);
+            }
+        }
+
+        public void OnSlotFilled(RequestEvent requestEvent, EthAddress host, BigInteger slotIndex)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                output.LogSlotFilled(requestEvent, host, slotIndex);
+            }
+        }
+
+        public void OnSlotFreed(RequestEvent requestEvent, BigInteger slotIndex)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                output.LogSlotFreed(requestEvent, slotIndex);
+            }
+        }
+
+        public void OnSlotReservationsFull(RequestEvent requestEvent, BigInteger slotIndex)
+        {
+            if (IsMyRequest(requestEvent))
+            {
+                output.LogSlotReservationsFull(requestEvent, slotIndex);
+            }
+        }
+
+        private bool IsMyRequest(RequestEvent requestEvent)
+        {
+            return requestId == requestEvent.Request.Request.Id.ToLowerInvariant();
         }
     }
+
 }
