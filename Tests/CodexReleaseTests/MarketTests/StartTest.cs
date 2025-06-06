0e4a6774ef71554001c34ddc1ae5dd4742aba4d6
ThatBen <thatbenbierens@gmail.com>
2025-06-06 09:49:55 +0200

    Convenient rerun attribute

--- Tests/CodexReleaseTests/MarketTests/StartTest.cs
+++ Tests/CodexReleaseTests/MarketTests/StartTest.cs
@@ -23,7 +23,7 @@ namespace CodexReleaseTests.MarketTests
         [Test]
         [Combinatorial]
         public void Start(
-            [Values([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])] int rerun
+            [Rerun] int rerun
         )
         {
             var hosts = StartHosts();
